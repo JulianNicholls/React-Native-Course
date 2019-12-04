2dd61c3279b880972c5fb1388ef0ab450eb34532
Julian Nicholls <juliannicholls29@gmail.com>
2019-12-04 16:24:51 +0000

    Restyle navigation buttons

--- first/src/screens/HomeScreen.js
+++ first/src/screens/HomeScreen.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
+    <View class={styles.buttonContainer}>
       <Text style={styles.title}>Home</Text>
-      <Button title="List" onPress={() => navigation.navigate('Lists')} />
-      <Button title="Images" onPress={() => navigation.navigate('Image')} />
-      <Button title="Counter" onPress={() => navigation.navigate('Counter')} />
-      <Button title="Colours" onPress={() => navigation.navigate('Colour')} />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Lists')}
+      >
+        <Text style={styles.buttonText}>List</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Image')}
+      >
+        <Text style={styles.buttonText}>Images</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Counter')}
+      >
+        <Text style={styles.buttonText}>Counter</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Colour')}
+      >
+        <Text style={styles.buttonText}>Colours</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -19,6 +39,19 @@ const styles = StyleSheet.create({
     fontSize: 30,
     textAlign: 'center',
     marginBottom: 10,
+  },
+  buttonContainer: {
+    display: 'flex',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#abd7ed',
+    padding: 10,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'blue',
+    fontSize: 20,
   },
 });
 
