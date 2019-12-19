451b3731a3134b3ea573cf7483baf000479c8ad3
Julian Nicholls <juliannicholls29@gmail.com>
2019-12-19 14:25:54 +0000

    Create Mongo User model

---
+++ track-server/src/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+mongoose.model('User', UserSchema);
