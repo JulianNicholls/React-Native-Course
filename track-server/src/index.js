9b0d54cb6b978adb96371da50fb9de245749c21b
Julian Nicholls <juliannicholls29@gmail.com>
2019-12-19 10:20:55 +0000

    Connect track server to local Mongo

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const port = process.env.PORT || 3001;
+
+const mongoUri = 'mongodb://localhost/track';
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to local Mongo');
+});
+
+mongoose.connection.on('error', err => {
+  console.error('Error connecting to local Mongo:', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Track-Server Root\n');
