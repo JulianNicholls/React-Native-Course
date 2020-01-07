bd9321b408f452381055a321a22b6b4bb56254d0
Julian Nicholls <juliannicholls29@gmail.com>
2020-01-07 15:09:22 +0000

    Ask for location permission and act on result

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-navigation';
 import { StyleSheet } from 'react-native';
+import { requestPermissionsAsync } from 'expo-location';
 import { Text } from 'react-native-elements';
 
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+  const [error, setError] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const response = await requestPermissionsAsync();
+      if (!response.granted) setError('denied');
+    } catch (err) {
+      console.log(err);
+      setError(err);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h2>Create a new track</Text>
       <Map />
+      {error !== null && (
+        <Text style={styles.error}>Please enable location services</Text>
+      )}
     </SafeAreaView>
   );
 };
@@ -17,6 +37,10 @@ const TrackCreateScreen = () => {
 const styles = StyleSheet.create({
   title: {
     fontSize: 32,
+  },
+  error: {
+    fontSize: 16,
+    color: 'red',
   },
 });
 
