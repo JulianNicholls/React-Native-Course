e70e106e31db004cb43b054721f10a4a8bbf3dbf
Julian Nicholls <juliannicholls29@gmail.com>
2020-01-09 11:13:09 +0000

    Update mock track to be less linear

--- tracks/src/_mockLocation.js
+++ tracks/src/_mockLocation.js
@@ -1,27 +1,35 @@
 import * as Location from 'expo-location';
 
 const tenMetersWithDegrees = 0.0001; // Appromimately 10m
+let prevLocation = { latitude: 50.733, longitude: -1.853 };
 
-const getLocation = increment => {
-  return {
-    timestamp: 100000000,
+const getLocation = () => {
+  const location = {
+    timestamp: Math.floor(Date.now() / 1000),
     coords: {
       speed: 0,
       heading: 0,
       accuracy: 5,
       altitudeAccuracy: 5,
       altitude: 5,
-      longitude: -1.83 + increment * tenMetersWithDegrees,
-      latitude: 50.733 + increment * tenMetersWithDegrees,
+      latitude: prevLocation.latitude,
+      longitude: prevLocation.longitude,
     },
   };
-};
 
-let counter = 0;
+  if (Math.random() > 0.4) location.coords.latitude += tenMetersWithDegrees;
+  if (Math.random() > 0.4) location.coords.longitude += tenMetersWithDegrees;
+
+  prevLocation = location.coords;
+
+  // console.log({ prev: prevLocation, coords: location.coords });
+
+  return location;
+};
 
 setInterval(() => {
   Location.EventEmitter.emit('Expo.locationChanged', {
     watchId: Location._getCurrentWatchId(),
-    location: getLocation(++counter),
+    location: getLocation(),
   });
 }, 2000);
