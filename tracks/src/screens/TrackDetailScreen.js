09511c73c43276aa371789d96247073efa3994a7
Julian Nicholls <juliannicholls29@gmail.com>
2020-01-13 12:06:01 +0000

    Add map and track to track detail screen

--- tracks/src/screens/TrackDetailScreen.js
+++ tracks/src/screens/TrackDetailScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
+import MapView, { Polyline } from 'react-native-maps';
 
 import { useTracks } from '../context/Tracks';
 
@@ -11,11 +12,34 @@ const TrackDetailScreen = ({ navigation }) => {
   } = useTracks();
   const trackId = navigation.getParam('_id');
   const track = tracks.find(({ _id }) => _id === trackId);
+  const locations = track.locations.map(({ coords }) => coords);
 
-  return <Text h3>{track.name}</Text>;
+  console.log({ length: locations.length, 0: locations[0] });
+  return (
+    <>
+      <Text h3>{track.name}</Text>
+
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: locations[0].latitude,
+          longitude: locations[0].longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Polyline coordinates={locations} />
+      </MapView>
+    </>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    marginTop: 15,
+    height: 300,
+  },
+});
 
 TrackDetailScreen.propTypes = {
   navigation: PropTypes.object.isRequired,
